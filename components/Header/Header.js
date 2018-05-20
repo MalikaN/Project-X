83321aad447d68b1f14517124ef15e1f3d7a1f73
indee89@gmail.com <indee89@gmail.com>
2018-05-21 00:39:32 +0530

    authnetication added

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -5,53 +5,66 @@ import styles from './HeaderStyle'
 
 class Header extends Component{
 
-    handleClick(){
-        this.Greeting()
+    state = {
+        userToken: ''
     }
-    UserGreeting() {
-        return <Link href=""><a onClick={this.handleClick()}>Logout</a></Link>;
-      }
-      
-    GuestGreeting() {
+
+    componentDidMount() {
+        this.setState({
+            userToken: jscookie.get('token')
+        })
+    }
+
+    logout=()=>{
+        this.setState({
+            userToken: ''
+        })
+        console.log('logoout',this.state.userToken)
         jscookie.remove('token')
-        console.log('logout',this.cookie)  
-        return <Link href="/login" ><a>Login</a></Link>;
-      }
-    Greeting() {
-        const cookie = jscookie.get('token')
-        
-        if (cookie) {
-          return this.UserGreeting();
-        }
-  
-        return this.GuestGreeting();
-         
-      }
+    }
+    
     render(){
+
+        const { userToken } = this.state;
+        console.log('Token',userToken)
         return(
             <div> 
-            <div className="menu">
+                <div className="menu">
                 <div className="logo">
                 <Link href="/" >
                         <a className="title">Project X</a>
                     </Link>
                 </div>
+
                 <div className="submenu">
-                    <div className="addpost">
-                        <Link href="/add-post" >
-                            <a>Add Post</a>
-                        </Link>
-                    </div>
-                    <div className="auth">
-                        {this.Greeting()}
-                    </div>
-                    <div className="signup">
-                        <Link href="/signup">
-                            <a >Signup</a>
-                        </Link>
-                    </div>                 
-                </div>
-               
+                    {userToken ?
+                        <React.Fragment>
+                            <div className="addpost">
+                                <Link href="/add-post" >
+                                    <a>Add Post</a>
+                                </Link>
+                            </div>
+                            <div className="auth">
+                                <Link href="">
+                                    <a onClick={this.logout()}>
+                                        Logout
+                                    </a>
+                                </Link>
+                            </div>
+                        </React.Fragment>
+                    :
+                        <React.Fragment>
+                            <div className="login">
+                                <Link href="/login" ><a>Login</a></Link>
+                            </div>
+                            <div className="signup">
+                                <Link href="/signup">
+                                    <a >Signup</a>
+                                </Link>
+                            </div>   
+                        </React.Fragment>
+                    }        
+                </div>  
             </div>
 
         <style jsx>{styles}</style>
