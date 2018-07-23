623f9c9e4379dd9f2f3d12def2aa2a87ec6c6f6e
Malika <malika@Tharakas-MacBook-Air.local>
2018-07-23 20:00:44 +0530

    changes index.js

--- pages/cards.js
+++ pages/cards.js
@@ -9,45 +9,44 @@ const Card =({post,index})=>{
         return(
             <Link href={`/post?id=${post.postId}`} >                       
             {index ?           
-                    <div className="cards__item" >
-                        <a className="link">
-                            <div className="outerCard">
-                                <div className="card">
-                                    <div className="imgOuterDiv">
-                                        <img src={post.PostSrc} alt="" className="card__image"/>
-                                    </div>                             
-                                    <div className="card__content">
-                                        <div className="card__title">{post.postTitle}</div>
-                                        <div className="card__text">{post.PostDesc}</div>                  
-                                    </div>
+                <div className="cards__item" >
+                    <a className="link">
+                        <div className="outerCard">
+                            <div className="card">
+                                <div className="imgOuterDiv">
+                                    <img src={post.PostSrc} alt="" className="card__image"/>
+                                </div>                             
+                                <div className="card__content">
+                                    <div className="card__title">{post.postTitle}</div>
+                                    <div className="card__text">{post.PostDesc}</div>                  
                                 </div>
                             </div>
-                        </a>
-                        <style jsx>{ indexStyles }</style>             
-                    </div>
+                        </div>
+                    </a>
+                    <style jsx>{ indexStyles }</style>             
+                </div>
             
             :
            
-            <li className="cards__item" key={post.postId}>
-            <div className="card">
-                <div className="imgOuterDiv">
-                    <Link href={`/post?id=${post.postId}`}>    
-                        <img src={post.PostSrc} alt="" className="card__image"/>
-                    </Link>
-                    <div className="edit">
-                        <Link href={`/edit-post?id=${post.postId}`}>
-                            <a><FontAwesomeIcon icon={ faPencilAlt }/></a>
-                        </Link>
-                    </div>                                       
-                </div>                             
-                <div className="card__content">
-                    <div className="card__title">{post.postTitle}</div>
-                    <div className="card__text">{post.PostDesc}</div>                  
-                </div>
-            </div>
-            <style jsx>{ myPostStyles }</style>
-        </li>                                  
-        
+                <li className="cards__item" key={post.postId}>
+                    <div className="card">
+                        <div className="imgOuterDiv">
+                            <Link href={`/post?id=${post.postId}`}>    
+                                <img src={post.PostSrc} alt="" className="card__image"/>
+                            </Link>
+                            <div className="edit">
+                                <Link href={`/edit-post?id=${post.postId}`}>
+                                    <a><FontAwesomeIcon icon={ faPencilAlt }/></a>
+                                </Link>
+                            </div>                                       
+                        </div>                             
+                        <div className="card__content">
+                            <div className="card__title">{post.postTitle}</div>
+                            <div className="card__text">{post.PostDesc}</div>                  
+                        </div>
+                    </div>
+                    <style jsx>{ myPostStyles }</style>
+                </li>                                    
             }   
             </Link>       
                 
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import {Component} from 'react'
 import withLayout from '../components/Layouts/Layout'
 import Link from 'next/link'
-import styles from './indexStyle'
+// import styles from './indexStyle'
 import axios from 'axios'
 import Card from './cards'
 import FontAwesomeIcon from '@fortawesome/react-fontawesome'
@@ -35,13 +35,13 @@ class Index extends Component{
         return(
             <div className="mainDiv">     
                 <div className="outerImagesDIv">
-                <div className="innerImagesDiv">
-                        <img src="http://farm6.staticflickr.com/5145/5576437826_940f2db110.jpg" alt="Image 1" />
-                        <img src="https://www.ybca.org/cms/2018/04/arch_des_series_2400-1440x960.jpg" alt="Image 2" />
-                        <img src="https://placeimg.com/640/480/nature" alt="Image 3" />
-                        <img src="http://farm2.staticflickr.com/1415/983021323_8eb2f92c01.jpg" alt="Image 4" />
-                        <img src="https://i.pinimg.com/originals/11/b3/cc/11b3ccbc57526e3c6978920ef697efc5.jpg" alt="Image 5" />
-                </div>
+                    <div className="innerImagesDiv">
+                            <img src="http://farm6.staticflickr.com/5145/5576437826_940f2db110.jpg" alt="Image 1" />
+                            <img src="https://www.ybca.org/cms/2018/04/arch_des_series_2400-1440x960.jpg" alt="Image 2" />
+                            <img src="https://placeimg.com/640/480/nature" alt="Image 3" />
+                            <img src="http://farm2.staticflickr.com/1415/983021323_8eb2f92c01.jpg" alt="Image 4" />
+                            <img src="https://i.pinimg.com/originals/11/b3/cc/11b3ccbc57526e3c6978920ef697efc5.jpg" alt="Image 5" />
+                    </div>
                 </div>
                 <div className="button-bar-overflow">
                     <div className="button-bar">
@@ -57,27 +57,24 @@ class Index extends Component{
                     </div>
                     <div>
                         <div className="cards">
-                            
-                                {childFilter.map(function(post,i){
+                            {childFilter.map(function(post,i){
                                     return(
                                         <Card key={post.postId} post={post} index={indexPage}/> 
                                     )
                                 })}  
-                               
                         </div>
                        {/* fr<div> <FontAwesomeIcon icon={ faChevronCircleRight }/> </div> */}
-                    </div>  
-                    
+                    </div>                   
                     <div>
                         <h3>Adults</h3>
                     </div>
                     <div>
-                    <ul className="cards">
-                        {adultFilter.map(function(post,i){
-                            return(
-                                <Card key={post.postId} post={post} index={indexPage}/>
-                            )
-                        })}       
+                        <ul className="cards">
+                            {adultFilter.map(function(post,i){
+                                return(
+                                    <Card key={post.postId} post={post} index={indexPage}/>
+                                )
+                            })}       
                         </ul>
                     </div>   
                     <div>
@@ -93,7 +90,7 @@ class Index extends Component{
                         </ul>
                     </div>             
                 </div>
-                <style jsx>{styles}</style>
+                {/* <style jsx>{styles}</style> */}
             </div>
         )
     }
