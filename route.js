dbe31477430410938b46cb53b647729e93e3961c
Malika <malika@Tharakas-MacBook-Air.local>
2018-08-12 13:38:30 +0530

    routes.js file changes

--- route.js
+++
@@ -1,5 +0,0 @@
-const routes = require('next-routes')
-
-                                                    // Name   Page      Pattern
-module.exports = routes()                           // ----   ----      -----
-.add('post', '/:slug/:id')                   // user   profile   /user/:id
--- routes.js
+++ routes.js
@@ -1,5 +1,4 @@
 const routes = require('next-routes')
-
-                                                    // Name   Page      Pattern
+                                                  // Name   Page      Pattern
 module.exports = routes()                           // ----   ----      -----
 .add('post', '/:slug/:id')                   // user   profile   /user/:id
