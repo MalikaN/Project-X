3a6fb44a49fc5a72a8f344f5b59a335b50449bb3
Malika <malika@Tharakas-MacBook-Air.local>
2018-08-12 17:05:06 +0530

    my-post page changes

--- pages/post.js
+++ pages/post.js
@@ -14,7 +14,7 @@ class Post extends Component{
         }
     
     componentWillMount(){
-        axios.get(`http://api.pihitak.com/post/${this.props.url.query.customCode}`)
+        axios.get(`http://api.pihitak.com/post/${this.props.url.query.CustomCode}`)
         .then((Response)=>{
            this.setState({
                 post:Response.data.Items[0]
